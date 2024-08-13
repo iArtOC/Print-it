5484bc1e23a2d5057a0ff137eab4fc8fa0e7b691
iArtOC <itrd.1rt+github@gmail.com>
2024-08-13 13:02:47 +0200

    feat:integrated arrows slider and bullet points

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,60 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let numero = 0
+
+//création du bouton gauche du slide
+var previousButton=document.createElement("div")
+previousButton.classList.add("arrow")
+previousButton.classList.add("arrow_left")
+//création de l'image du bouton du slide
+var previousButtonImg=document.createElement("img")
+previousButtonImg.src="./assets/images/arrow_left.png"
+previousButton.append(previousButtonImg)
+//affichage du bouton dans le DOM
+var banner=document.getElementById("banner")
+banner.append(previousButton)
+//EventListener du button gauche
+previousButton.addEventListener('click', function(){
+	changeSlide (-1)
+})
+
+
+//création du bouton droit du slide
+var nextButton=document.createElement("div")
+nextButton.classList.add("arrow")
+nextButton.classList.add("arrow_right")
+//création de l'image du bouton du slide
+var nextButtonImg=document.createElement("img")
+nextButtonImg.src="./assets/images/arrow_right.png"
+nextButton.append(nextButtonImg)
+//affichage du bouton dans le DOM
+var banner=document.getElementById("banner")
+banner.append(nextButton)
+//EventListener du button droite
+nextButton.addEventListener('click', function(){
+	changeSlide (+1)
+})
+
+//création des points bulles
+var dots=document.querySelector(".dots")
+for (var slide of slides){
+	var dot=document.createElement("div")
+	dot.classList.add("dot")
+	dots.append(dot)
+}
+
+//fonction de changement image
+function changeSlide (direction){
+	/*numero = numero + direction*/
+	/*document.getElementById("banner").src= "assets/images/slideshow" + slides[numero];*/
+	console.log(slides[2])
+	if (direction==="previous"){
+		console.log("avant")
+	} else {
+		console.log("après")
+	}
+}
+
+setInterval("changeSlide(1)", 4000)
